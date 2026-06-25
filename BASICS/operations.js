24f9e387b3e178444d4d803edf419a24bdd6ba3f
shreyasingh272004 <singhshreya272004@gmail.com>
2026-06-25 18:43:31 +0000

    Fifth Commit

---
+++ BASICS/operations.js
@@ -0,0 +1,40 @@
+let val=3;
+let negVal=-val;
+console.log(val,negVal);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);//power
+console.log(2%2);
+
+let str1="hello"
+let str2=" shreya"
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);//12
+
+//IMP TRICKY PART
+console.log("1"+2+3);//123 
+console.log(1+2+"3");//33
+
+console.log(true);//true
+console.log(+true);//1
+//console.log(true+);//error
+console.log(+"");//0
+console.log(2>3);//false nd there r more comparisons like < >= <= != ==
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.log(num1,num2,num3);
+
+let cnt=100
+console.log(cnt);
+cnt++;
+console.log(cnt);
+
+//=== checks datatype
+console.log("2"===2);//false
+
