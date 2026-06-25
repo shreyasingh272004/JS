a7bb508cae74d6ee21a8366761c36c22237f638e
shreyasingh272004 <singhshreya272004@gmail.com>
2026-06-25 18:25:00 +0000

    Third Commit

---
+++ BASICS/data_types.js
@@ -0,0 +1,9 @@
+"use strict";//treat all js code as newer version!  tho u dont need to write it everytime jst know that whatver code yur writing will automatically run in stricter version
+
+//alert(3+3);// it wont work rn bvz we're using node.js nd not browser
+
+
+console.log(3+1); 
+
+console.log("Shreya");
+
