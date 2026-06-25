7a63b5714f453a3048f1979fcfc335a0cc630c07
shreyasingh272004 <singhshreya272004@gmail.com>
2026-06-25 18:02:09 +0000

    2nd commit

--- BASICS/test.js
+++ BASICS/test.js
@@ -1,1 +1,20 @@
 console.log("Hello");
+
+const accountId=12342;//const variable cant be changed once declared
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"//
+accountCity="Jaipur"
+
+
+accountEmail="singh@gmail.com";
+accountPassword="12323"
+accountCity="varanasi"
+let accountState;
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);//account table helps in printing multiple variables output at once
+
+//const and let is most used variable and DO NOT USE VAR bcz var casues issue in block scope
+
+
+
+
