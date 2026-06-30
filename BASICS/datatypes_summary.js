0ec30b70bf91b030e93ede0f50a652c3c3545fd8
shreyasingh272004 <singhshreya272004@gmail.com>
2026-06-30 17:26:52 +0000

    Fifth Commit

---
+++ BASICS/datatypes_summary.js
@@ -0,0 +1,46 @@
+const score=100
+const scoreVal=100.3
+const login=false;
+const bigNumber=324054983982903019203n
+
+
+
+
+
+const fruits=["apple","grapes","banana"];
+
+let myObj={
+    name:"shreya",
+    age:22,
+}
+
+const myfunc=function(){
+    console.log("hello world");
+}
+
+console.log(typeof myfunc)
+
+
+/* memory types-stack,heap
+    stack memory is used with primitive datatypes
+    heap memory is used with non-primitive datatypes
+*/
+
+let ytname="singhshreya"
+
+let anothername=ytname
+anothername="chaiaurcode"
+
+console.log(anothername);
+
+let user={
+    email:"user@gmail.com",
+    upi:"user@bankname",
+}
+
+let userTwo=user
+
+userTwo.email="singhshreya@gmail.com"
+console.log(userTwo.email);
+console.log(userTwo.email);
+
---
+++ BASICS/strings.js
@@ -0,0 +1,6 @@
+const name="shreya"
+const age=1021901
+
+console.log(name+age);
+
+console.log(`Hello my name is ${name} and i'm ${age} years old`);
