a142caa1c66343b92f51b71fadfa9b60e41580c0
shreyasingh272004 <singhshreya272004@gmail.com>
2026-06-30 17:37:16 +0000

    Sixth Commit

--- BASICS/strings.js
+++ BASICS/strings.js
@@ -4,3 +4,16 @@ const age=1021901
 console.log(name+age);
 
 console.log(`Hello my name is ${name} and i'm ${age} years old`);
+
+
+
+let s="shreya";
+
+console.log(s.toUpperCase());
+console.log(s.length);
+console.log(s.charAt(2));
+console.log(s.indexOf('s'));
+
+
+
+
